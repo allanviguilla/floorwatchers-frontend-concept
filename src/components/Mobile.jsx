17144a8120b95b60a1568c83d844431d7e6527d4
allanviguilla <viguilla.allan@gmail.com>
2022-10-01 00:56:34 -0700

    Fix mobile menu

--- src/components/Mobile.jsx
+++ src/components/Mobile.jsx
@@ -8,20 +8,16 @@ import styles from '../App.module.css';
 export default function Mobile({ setShowMobile }) {
   return(
     <div className={styles.mobile}>
-      <div className={styles.mobileNav}>
-        <button
-          className={styles.closeWindow}
-          onClick={() => {setShowMobile(false)}}>
-          <span className={styles.closeIcon}>X</span>
-        </button>
-      </div>
-      <div className={styles.mobileBody}>
-        <a href="https://medium.com/@FloorWatchersSociety/floor-watchers-society-season-1-playbook-6eca89cebcbf" target="_blank">
+      <button
+        className={styles.closeWindow}
+        onClick={() => {setShowMobile(false)}}>
+        <span className={styles.closeIcon}>X</span>
+      </button>
+      <a className={styles.wpLink} href="https://medium.com/@FloorWatchersSociety/floor-watchers-society-season-1-playbook-6eca89cebcbf" target="_blank">
           <h3>Read the Whitepaper</h3>
-          <a href="https://twitter.com/FloorWatchers" target="_blank"><img src={twitter} /></a>
-          <a href="" target="_blank"><img src={discord} /></a>
         </a>
-      </div>
+        <a href="https://twitter.com/FloorWatchers" target="_blank"><img src={twitter} /></a>
+        <a href="" target="_blank"><img src={discord} /></a>
     </div>
   );
 }
